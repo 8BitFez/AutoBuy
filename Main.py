e232bf49c48ca1b844c05ab3b0e07dffb86d13aa
sully <5883548+8BitFez@users.noreply.github>
2021-03-24 13:04:02 -0500

    switching to gamestop

--- Main.py
+++ Main.py
@@ -8,16 +8,17 @@ exitcode = 0
 print("going to sleep")
 web.go_to('google.com')
 soldOut = True
-searchItem = "Nest Mini (2nd Generation) Smart Speaker with Google Assistant - Chalk"
- #"Microsoft - Xbox Series X 1TB Console - Black"
-
+searchItem = "Xbox Series X"
+webstore = "www.gamestop.com"
+#
+#"Nest Mini (2nd Generation) Smart Speaker with Google Assistant - Chalk"
 #setting up menu to grab cookies and help with prototyping
 
 while exitcode == 0:
     print("What would you like to do")
     print ('import cookies: 1 /n export cookies: 2 /n run loop: 3 /n quit: quit ')
-    # a = '3'
-    a = input("what would you like to do? ")
+    a = '3'
+    #a = input("what would you like to do? ")
     if a == '1':
         print("Importing Cookies!")
         cookies = pickle.load(open("cookies.pkl", "rb"))
@@ -45,23 +46,25 @@ while exitcode == 0:
     #Starting Scripts for right just looping once
 
         print("Running Bot! Time: " + str(asctime()))
-        web.go_to('bestbuy.com')
+        web.go_to(webstore)
         web.press(web.Key.ESCAPE)
-        web.type( searchItem ,id='gh-serch-input')
+        web.type( searchItem , classname= 'search-field')
         web.press(web.Key.ENTER)
-        web.click(text=searchItem)
+        web.click(text=searchItem, loose_match=False)
         # we are now just  waiting for one to be in stock
         #so we will read the html page to see if some are in stock
         while soldOut:
             
-            soldOut = web.exists(text="Sold Out",tag="button",classname="btn-disable")
+            soldOut = web.exists(text="Sold Out",tag="button")
             print("Are they sold out? "+ str(soldOut) )
             sleep(5)
             web.refresh()
+
         # xbox is in stock now 
         print(searchItem + " is in stock! Buying!")
+        g = input("halting!")
         #web.click()
-        #exitcode += 1
+        exitcode += 1
         
 
 
